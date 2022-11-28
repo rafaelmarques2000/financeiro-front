fa28c0ce298c6cb38572009a8bbdf880a2e4e3cf
rafael <rafaelmarquespaixao@outlook.com>
2022-11-28 11:40:20 -0300

    build

--- src/service/utils/helpers.js
+++ src/service/utils/helpers.js
@@ -7,15 +7,22 @@ const getMoneyValue = (value) => {
         return value * 100
     }else {
         let stringMoney = String(value).split(".")
-        let decimal = parseInt(stringMoney[1])
-
-        let money = String(value)
-                            .replace(",", "")
-                            .replace(".", "")
-        if(decimal < 10) {
-            money = money.concat('0')
+        let integer = stringMoney[0]
+        let decimal = stringMoney[1]
+        let result = 0
+        if(decimal.indexOf('0',0) < 0) {
+            if(parseInt(decimal) < 10) {
+                result = integer
+                    .concat(decimal)
+                    .concat('0')
+            }else {
+                result = integer
+                    .concat(decimal)
+            }
+        }else{
+            result = integer.concat(decimal)
         }
-        return Number.parseInt(money)
+        return Number.parseInt(result)
     }
 }
 
